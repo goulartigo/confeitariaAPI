747c09dca6257160165986759d55088a598fbc38
goularto2020 <goularto2020@gmail.com>
2025-11-29 00:34:49 +0000

    conexao

--- conexao.py
+++ conexao.py
@@ -3,7 +3,7 @@ import psycopg2
 def get_conexao():
     conn = psycopg2.connect(
         dbname='postgres',
-        user='postgres.mliaizbqrzstuzleuqvf',
+        user='postgres.royfkjapbmqrdoprlydy',
         password='CapivariIfsp',
         host='aws-1-us-east-2.pooler.supabase.com',
         port=6543
